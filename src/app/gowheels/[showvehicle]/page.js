c962b10f7ba1a5bc1f7047f258ad983cd2415664
gourav4ojha <gouravvishwakarma049@gmail.com>
2025-04-10 19:50:30 +0530

    host

--- src/app/gowheels/[showvehicle]/page.js
+++ src/app/gowheels/[showvehicle]/page.js
@@ -33,56 +33,56 @@ export default async function showvehicles({ params }) {
     
     const amount = vehicle.amount;
     
-    const handlePayment = async () => {
-        if (!amount) {
-            console.error("Amount is not defined");
-            return;
-        }
+    // const handlePayment = async () => {
+    //     if (!amount) {
+    //         console.error("Amount is not defined");
+    //         return;
+    //     }
     
-        if (!process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID) {
-            console.error("Razorpay Key ID is missing!");
-            return;
-        }
+    //     if (!process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID) {
+    //         console.error("Razorpay Key ID is missing!");
+    //         return;
+    //     }
     
-        try {
-            const response = await fetch(`/create-order`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ amount }),
-            });
+    //     try {
+    //         const response = await fetch(`/create-order`, {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({ amount }),
+    //         });
     
-            const { data } = await response.json();
-            console.log("Order ID:", data.id);
+    //         const { data } = await response.json();
+    //         console.log("Order ID:", data.id);
     
-            const options = {
-                key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-                amount: amount * 100,
-                currency: "INR",
-                name: "Campus Rentals",
-                description: "Test Transaction",
-                order_id: data.id,
-                handler: function (response) {
-                    console.log("Payment Successful:", response);
-                    alert("Payment Successful!");
-                },
-                prefill: {
-                    name: "John Doe",
-                    email: "johndoe@example.com",
-                    contact: "9999999999",
-                },
-                theme: {
-                    color: "#3399cc",
-                },
-            };
+    //         const options = {
+    //             key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+    //             amount: amount * 100,
+    //             currency: "INR",
+    //             name: "Campus Rentals",
+    //             description: "Test Transaction",
+    //             order_id: data.id,
+    //             handler: function (response) {
+    //                 console.log("Payment Successful:", response);
+    //                 alert("Payment Successful!");
+    //             },
+    //             prefill: {
+    //                 name: "John Doe",
+    //                 email: "johndoe@example.com",
+    //                 contact: "9999999999",
+    //             },
+    //             theme: {
+    //                 color: "#3399cc",
+    //             },
+    //         };
     
-            const paymentObject = new window.Razorpay(options);
-            paymentObject.open();
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    };
+    //         const paymentObject = new window.Razorpay(options);
+    //         paymentObject.open();
+    //     } catch (error) {
+    //         console.error("Error:", error);
+    //     }
+    // };
     
 
     return (
