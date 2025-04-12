f4f492f765f752d590c00d6061abef8e02b21b94
gourav4ojha <gouravvishwakarma049@gmail.com>
2025-04-12 14:30:35 +0530

    host

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -1,39 +1,29 @@
-import React from 'react';
-import Tabs from '@/components/tabs';
+// components/Place.js
 
-const Place = () => {
-  return (
-    <div id="deals" className="relative p-4 mb-4">
-      {/* <div className="flex justify-center items-center  p-3">
-        <button className="mt-[90px] mb-4 bg-orange-200 hover:bg-orange-600 px-4 py-1 rounded-2xl text-orange-500 hover:text-white h-8 w-28 font-black text-xs uppercase cursor-pointer ">
-          deals
-        </button>
-      </div> */}
-      <div className="relative ">
-        <div className="flex flex-col justify-center items-center">
-          <p className=" text-[140px] sm:text-[180px] text-blue-700 opacity-5 font-black text-center z-0 absolute top-0 left-0 w-full uppercase dark:text-white">
-            deals
-          </p>
-          <p className="text-4xl text-blue-900 font-bold text-center z-20 relative capitalize mt-20 sm:mt-28 dark:text-gray-300">
-            our best deals for today
-          </p>
-        </div>
-      </div>
+export default {
+  async searchByText(request) {
+    const mockPlaces = [
+      {
+        displayName: "Tasty Tacos",
+        location: { latitude: 37.4219999, longitude: -122.0840575 },
+        businessStatus: "OPERATIONAL",
+      },
+      {
+        displayName: "El Taco Loco",
+        location: { latitude: 37.4224764, longitude: -122.0842499 },
+        businessStatus: "OPERATIONAL",
+      },
+      {
+        displayName: "Mountain Taco House",
+        location: { latitude: 37.4230012, longitude: -122.0852073 },
+        businessStatus: "OPERATIONAL",
+      },
+    ];
 
-      <div className="relative justify-center items-center">
-        <p className="font-medium text-base text-center mt-20  text-bluePText">
-          {/* Real estate is &quot;property consisting of land and the buildings on
-          <br />
-          it, along with its natural resources such as crops,
-          <br /> minerals or water, immovable property of this nature&quot; */}
-        </p>
-      </div>
+    const filtered = mockPlaces
+      .filter((place, index) => index < (request.maxResultCount || 10))
+      .filter((place) => request.minRating ? 4 >= request.minRating : true); // All mocked places have rating >= 4
 
-      <div className="mt-14">
-        <Tabs />
-      </div>
-    </div>
-  );
+    return { places: filtered };
+  },
 };
-
-export default Place;
